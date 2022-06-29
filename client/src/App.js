c542ee7c75dd92b1c17ab337ad6714a37d07e4c3
edricgalentino <erdrikpertama@gmail.com>
2022-06-29 11:59:16 +0700

    fixing routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-// import react router dom
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
     return (
         <div className="App overflow-hidden transition-all font-main">
@@ -12,7 +16,11 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
                     <Route path="/cart" element={<Cart />} />
+                    <Route path="/list-product" element={<ProductList />} />
+                    <Route path="/detail-product" element={<ProductDetail />} />
                 </Routes>
                 <Footer />
             </Router>
