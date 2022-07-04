7754c52f8719899851c9a7e2ff76ee0ba1c58519
edricgalentino <erdrikpertama@gmail.com>
2022-07-04 14:21:53 +0700

    pay page

---
+++ client/src/pages/Pay.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pay = () => {
+    return (
+        <>
+            <div className="flex justify-center items-center h-screen w-full">
+                <span className=" bg-black p-2 px-9 rounded-full text-white hover:ring-4 hover:ring-slate-400 transition-all">Pay</span>
+            </div>
+        </>
+    );
+};
+
+export default Pay;
