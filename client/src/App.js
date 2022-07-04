f9999c9bce3520ff77cb67a6f9b0a12fc326fdf2
edricgalentino <erdrikpertama@gmail.com>
2022-07-04 14:21:48 +0700

    payment route

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Pay from "./pages/Pay";
+import PaymentSuccess from "./pages/PaymentSuccess";
 
 function App() {
     return (
@@ -21,6 +23,8 @@ function App() {
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/list-product" element={<ProductList />} />
                     <Route path="/detail-product" element={<ProductDetail />} />
+                    <Route path="/payment/checkout" element={<Pay />} />
+                    <Route path="/payment/success" element={<PaymentSuccess />} />
                 </Routes>
                 <Footer />
             </Router>
